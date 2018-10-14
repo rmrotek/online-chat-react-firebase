a3a385415a494103e5e92ebaedf159fe0da8545a
rmrotek <reme87@gmail.com>
2018-10-14 12:00:56 +0200

    form.js template and state

---
+++ src/components/Form/Form.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Message from '../Message/Message';
+
+import firebase from 'firebase';
+
+
+class Form extends Component {
+
+  state = {
+    userName: 'Bob',
+    message: '',
+    list: [],
+  }
+
+
+  render(){
+    return(
+      <>
+      </>
+    )
+  }
+}
+
+
+
+export default Form
