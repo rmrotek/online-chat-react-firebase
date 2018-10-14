4acc20571715738c9fd06157affc26f03f046c37
rmrotek <reme87@gmail.com>
2018-10-14 11:38:23 +0200

    firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAfuEFWmsjU7AifzBLomYYd459Pz02zysE",
+  authDomain: "online-chat-6203d.firebaseapp.com",
+  databaseURL: "https://online-chat-6203d.firebaseio.com",
+  projectId: "online-chat-6203d",
+  storageBucket: "online-chat-6203d.appspot.com",
+  messagingSenderId: "107912395345"
+};
+firebase.initializeApp(config);
+
+const db = firebase.database();
