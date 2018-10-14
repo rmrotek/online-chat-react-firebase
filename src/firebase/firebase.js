8d9ddd65208ceb7f22db9235ab21230112a84813
rmrotek <reme87@gmail.com>
2018-10-14 12:05:34 +0200

    added fb config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,11 @@
+
+
+
+export default {
+  apiKey: "AIzaSyAfuEFWmsjU7AifzBLomYYd459Pz02zysE",
+  authDomain: "online-chat-6203d.firebaseapp.com",
+  databaseURL: "https://online-chat-6203d.firebaseio.com",
+  projectId: "online-chat-6203d",
+  storageBucket: "online-chat-6203d.appspot.com",
+  messagingSenderId: "107912395345"
+};
