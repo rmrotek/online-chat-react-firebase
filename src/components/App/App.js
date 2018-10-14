442102fe73384a0e43b3904a50a87c7b2fe2436d
rmrotek <reme87@gmail.com>
2018-10-14 12:14:06 +0200

    app prepared

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Form from '../Form/Form'
 
 
 class App extends Component {
@@ -11,10 +11,21 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className='App-header'>
+      <div className="app">
+        <div className='app-header'>
           <h1>Online chat</h1>
           <h4>React + Firebase</h4>
+        </div>
+
+        <div className='app-user-login'>
+          <button>Sign in</button>
+          <button>Logout</button>
+
+        </div>
+
+        <div className='app-main'>
+          <Form />
+
         </div>
 
 
